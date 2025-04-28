c3fab9a7b69b33dc3ef5e9290445cb858771ce09
mdsalathossain0 <mdsalathosain0@gmail.com>
2025-04-28 11:53:50 +0600

    Gallary Part Done

--- src/layouts/Gallary.jsx
+++ src/layouts/Gallary.jsx
@@ -12,11 +12,11 @@ const Gallary = () => {
   return (
     <section className='bg-second py-12'>
         <Container>
-            <Flex className='justify-between pb-5'>
-                <h2 className='text-4xl text-first font-semibold font-inter'>Galleries</h2>
-                <span className='text-xl text-third font-semibold font-inter cursor-pointer'>view all</span>
+            <Flex className='justify-between pb-5  '>
+                <h2 className='text-2xl sm:text-3xl px-3 md:px-0 md:text-4xl text-first font-semibold font-inter'>Galleries</h2>
+                <span className='text-lg px-3 md:px-0 md:text-xl text-third font-semibold font-inter cursor-pointer'>view all</span>
             </Flex>
-            <Flex className='justify-between'>
+            <Flex className='md:justify-between flex-wrap justify-center gap-8 md:gap-0 py-5 md:py-0'>
                 <div className='w-[280px] h-[350px] rounded-[28px]'>
                     <Image className='w-full h-full' src={GallaryOne}/>
                 </div>
